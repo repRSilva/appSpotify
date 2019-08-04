e693bf00807ff618483f098aa8c4fe4ec80e42fe
Rafael <rafaelsilvamap@gmail.com>
2019-08-04 16:53:34 -0300

    Ajuster sliders

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -3,114 +3,130 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
+import { Creators as PlayerActions } from '../../store/ducks/player';
 import Loading from '../../components/Loading';
-import { Container, Header, SongList } from './styles';
+import { Container, Header, SongList, SongItem } from './styles';
 import ClockIcon from '../../assets/images/clock.svg';
 import PlusIcon from '../../assets/images/plus.svg';
 
 class Playlist extends Component {
-	static propTypes = {
-		match: PropTypes.shape({
-			params: PropTypes.shape({
-				id: PropTypes.number
-			})
-		}).isRequired,
-		getPlaylistDetailsRequest: PropTypes.func.isRequired,
-		playlistDetails: PropTypes.shape({
-			data: PropTypes.shape({
-				thumbnail: PropTypes.string,
-				title: PropTypes.string,
-				description: PropTypes.string,
-				songs: PropTypes.arrayOf(
-					PropTypes.shape({
-						id: PropTypes.number,
-						title: PropTypes.title,
-						author: PropTypes.string,
-						album: PropTypes.string
-					})
-				)
-			}),
-			loading: PropTypes.bool
-		}).isRequired
-	};
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.shape({
+                id: PropTypes.number
+            })
+        }).isRequired,
+        getPlaylistDetailsRequest: PropTypes.func.isRequired,
+        playlistDetails: PropTypes.shape({
+            data: PropTypes.shape({
+                thumbnail: PropTypes.string,
+                title: PropTypes.string,
+                description: PropTypes.string,
+                songs: PropTypes.arrayOf(
+                    PropTypes.shape({
+                        id: PropTypes.number,
+                        title: PropTypes.title,
+                        author: PropTypes.string,
+                        album: PropTypes.string
+                    })
+                )
+            }),
+            loading: PropTypes.bool
+        }).isRequired,
+        loadSong: PropTypes.func.isRequired,
+        currentSong: PropTypes.shape({
+            id: PropTypes.number
+        }).isRequired
+    };
 
-	componentDidMount() {
-		this.loadPlaylistDetails();
-	}
+    state = {
+        selectedSong: null
+    };
 
-	componentDidUpdate(prevProps) {
-		if (prevProps.match.params.id !== this.props.match.params.id) {
-			this.loadPlaylistDetails();
-		}
-	}
+    componentDidMount() {
+        this.loadPlaylistDetails();
+    }
 
-	loadPlaylistDetails = () => {
-		const { id } = this.props.match.params;
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.id !== this.props.match.params.id) {
+            this.loadPlaylistDetails();
+        }
+    }
 
-		this.props.getPlaylistDetailsRequest(id);
-	};
+    loadPlaylistDetails = () => {
+        const { id } = this.props.match.params;
 
-	renderDetails = () => {
-		const playlist = this.props.playlistDetails.data;
-		return (
-			<Container>
-				<Header>
-					<img src={playlist.thumbnail} Alt={playlist.title} />
-					<div>
-						<span>PLAYLIST</span>
-						<h1>{playlist.title}</h1>
-						{!!playlist.songs && <p>{playlist.songs.length} Músicas</p>}
-						<button>PLAY</button>
-					</div>
-				</Header>
-				<SongList cellPadding={0} cellSpacing={0}>
-					<thead>
-						<th />
-						<th>Título</th>
-						<th>Artista</th>
-						<th>Álbum</th>
-						<th>
-							<img src={ClockIcon} alt="Duração" />
-						</th>
-					</thead>
-					<tbody>
-						{!playlist.songs ? (
-							<tr>
-								<td colSpan={5}>Nenhuma música cadastrada</td>
-							</tr>
-						) : (
-							playlist.songs.map((song) => (
-								<tr key={song.id}>
-									<td>
-										<img src={PlusIcon} alt="Adicionar" />
-									</td>
-									<td>{song.title}</td>
-									<td>{song.author}</td>
-									<td>{song.album}</td>
-									<td>3:26</td>
-								</tr>
-							))
-						)}
-					</tbody>
-				</SongList>
-			</Container>
-		);
-	};
+        this.props.getPlaylistDetailsRequest(id);
+    };
 
-	render() {
-		return this.props.playlistDetails.loading ? (
-			<Container loading>
-				<Loading />
-			</Container>
-		) : (
-			this.renderDetails()
-		);
-	}
+    renderDetails = () => {
+        const playlist = this.props.playlistDetails.data;
+        return (
+            <Container>
+                <Header>
+                    <img src={playlist.thumbnail} Alt={playlist.title} />
+                    <div>
+                        <span>PLAYLIST</span>
+                        <h1>{playlist.title}</h1>
+                        {!!playlist.songs && <p>{playlist.songs.length} Músicas</p>}
+                        <button>PLAY</button>
+                    </div>
+                </Header>
+                <SongList cellPadding={0} cellSpacing={0}>
+                    <thead>
+                        <th />
+                        <th>Título</th>
+                        <th>Artista</th>
+                        <th>Álbum</th>
+                        <th>
+                            <img src={ClockIcon} alt="Duração" />
+                        </th>
+                    </thead>
+                    <tbody>
+                        {!playlist.songs ? (
+                            <tr>
+                                <td colSpan={5}>Nenhuma música cadastrada</td>
+                            </tr>
+                        ) : (
+                                playlist.songs.map((song) => (
+                                    <SongItem
+                                        key={song.id}
+                                        onClick={() => this.setState({ selectedSong: song.id })}
+                                        onDoubleClick={() => this.props.loadSong(song, playlist.songs)}
+                                        selected={this.state.selectedSong === song.id}
+                                        playing={this.props.currentSong && this.props.currentSong.id === song.id}
+                                    >
+                                        <td>
+                                            <img src={PlusIcon} alt="Adicionar" />
+                                        </td>
+                                        <td>{song.title}</td>
+                                        <td>{song.author}</td>
+                                        <td>{song.album}</td>
+                                        <td>3:26</td>
+                                    </SongItem>
+                                ))
+                            )}
+                    </tbody>
+                </SongList>
+            </Container>
+        );
+    };
+
+    render() {
+        return this.props.playlistDetails.loading ? (
+            <Container loading>
+                <Loading />
+            </Container>
+        ) : (
+                this.renderDetails()
+            );
+    }
 }
 const mapStateToProps = (state) => ({
-	playlistDetails: state.playlistDetails
+    playlistDetails: state.playlistDetails,
+    currentSong: state.player.currentSong
 });
 
-const mapDispatchToProps = (dispatch) => bindActionCreators(PlaylistDetailsActions, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ ...PlaylistDetailsActions, ...PlayerActions }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Playlist);
--- src/pages/playlist/styles.js
+++ src/pages/playlist/styles.js
@@ -74,11 +74,17 @@ export const SongList = styled.table`
 			text-align: right;
 		}
 	}
-	tbody td {
+`;
+
+export const SongItem = styled.tr`
+	td {
 		border-top: 1px solid #282828;
 		font-size: 13px;
 		padding: 0 10px;
 		line-height: 40px;
+		background: ${(props) => (props.selected ? '#282828' : 'transparent')};
+		color: ${(props) => (props.playing ? '#1ed760' : '#FFF')};
+
 		&:first-child {
 			width: 80px;
 			text-align: right;
@@ -87,7 +93,7 @@ export const SongList = styled.table`
 			text-align: right;
 		}
 	}
-	tbody tr:hover td {
+	&:hover td {
 		background: #282828;
 	}
 `;
